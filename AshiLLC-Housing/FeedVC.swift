b9e014d1c3529ca3f50b58496189fc5391d3d80e
Pranjal Kukreja <pranjalkukreja@Pranjals-MacBook-Pro.local>
2017-04-01 19:11:09 -0400

    Firebase get complete

--- AshiLLC-Housing/FeedVC.swift
+++ AshiLLC-Housing/FeedVC.swift
@@ -12,7 +12,7 @@ import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    
+    var posts = [Post]()
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -37,14 +37,23 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 3
+       
+        
+        
+        return posts.count
         
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+         let post = posts[indexPath.row]
+        print("Pranjal: \(post.caption)")
+        
+        
        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
     }
     
     
@@ -58,10 +67,19 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
-        
-        
     }
 
   
